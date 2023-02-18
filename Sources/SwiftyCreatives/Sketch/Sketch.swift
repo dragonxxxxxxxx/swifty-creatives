acbb733c7f67e12bc3587fe8f5999a1bdf0e6bf1
yukiny0811 <yukiny0811@gmail.com>
2023-02-18 15:49:15 +0900

    mainactor

--- Sources/SwiftyCreatives/Sketch/Sketch.swift
+++ Sources/SwiftyCreatives/Sketch/Sketch.swift
@@ -28,8 +28,14 @@ open class Sketch: SketchBase {
     public init() {}
     
     // MARK: functions
+    
+    @MainActor
     open func setupCamera(camera: some MainCameraBase) {}
+    
+    @MainActor
     open func update(camera: some MainCameraBase) {}
+    
+    @MainActor
     open func draw(encoder: SCEncoder) {}
     
     public func beforeDraw(encoder: SCEncoder) {
